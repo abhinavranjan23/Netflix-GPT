68e9936496e0dc00e4d62756801c0a146fe3e531
abhinavranjan23 <abhinavranjan2202@gmail.com>
2024-12-25 12:33:59 +0530

    only login user can see the broser page and if login then can not see login page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,21 +4,52 @@ import { auth } from "../utils/firebase";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { addUser, removeUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux";
 
 const Header = () => {
   const user = useSelector((store) => store.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
         // Sign-out successful.
-        navigate("/");
+        // navigate("/");
       })
       .catch((error) => {
         // An error happened.
         navigate("/error");
       });
   };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/broser");
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        navigate("/");
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
     <div className='absolute w-full pr-20  pl-32 bg-gradient-to-b from-black z-10  flex justify-between  items-center'>
       <img
